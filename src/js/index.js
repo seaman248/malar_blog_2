1e381b785711eb31d826ee547dc108ebaae4f182
seaman <semen.bko@ya.ru>
2015-03-31 00:19:28 +0300

    include masonry for adaptive thumbnails

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,18 @@
 global.jQuery = global.$ = require('jquery');
+$(window).load(function(){
+	// Navbar toogle button
+	$('.responsive_toggle_button button').click(function(){
+		$('.navbar').toggle('slow');
+	});
 
-// Navbar toogle button
-$('.responsive_toggle_button button').click(function(){
-	$('.navbar').toggle('slow');
-});
+	// Adaptive 
+	var Masonry = require('masonry-layout');
+
+	new Masonry ('.thumbs', {
+		columnWidth: '.thumbnail',
+		itemSelector: '.thumbnail',
+		isAnimated: true,
+		isResizable: true,
+		singleMode: true,
+	})
+})
